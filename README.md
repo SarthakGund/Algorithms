5d93b543180de47887c615c9d40f5108d52a010c
Sarthak Gund <sarthakdg03@gmail.com>
2025-05-15 18:12:47 +0530

    DSU

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1. Dijktra
 2. Floyd Warshall
 3. Bellman Ford
+4. Disjoint Set Union
---
+++ disjointsetunion.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define int long long
+#define endl "\n"
+
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+
+class DisjointSet{
+    private:
+    vector<int> rank, size, parent;
+
+    public:
+    int components, maxsize;
+    DisjointSet(int n){
+        components = n;
+        maxsize = 1;
+        rank.resize(n + 1);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for(int i = 0; i <= n; i++) parent[i] = i;
+    }
+
+    int findUpar(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findUpar(parent[node]);
+    }
+
+    void unionByRank(int u, int v){
+        int ulp_v = findUpar(v);
+        int ulp_u = findUpar(u);
+
+        if(ulp_v == ulp_u) return;
+        if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        }
+        else if(rank[ulp_u] > rank[ulp_v]){
+            parent[ulp_v] = parent[ulp_u];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v){
+        int ulp_u = findUpar(u);
+        int ulp_v = findUpar(v);
+
+        if(ulp_u == ulp_v) return;
+        if(size[ulp_v] < size[ulp_u]){
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+        else{
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+    }
+};
+
+void solve(){
+    
+}
+
+int32_t main() {
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
